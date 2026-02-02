4771dcf9d2bc8d3f8745e3ba602db1000292fa45
m41250gj <george.jones-6@student.manchester.ac.uk>
2026-02-02 17:08:56 +0000

    added an error message for n_i<n_f and added user prompts for all inputs

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -8,9 +8,17 @@ int main()
     {
         int z, n_i, n_f;
         std::string unit;
+        std::cout << "Seperated by a space: enter atomic number (Z), initial energy level (n_i), final energy level (n_f), and unit (J/e): ";
         std::cin >> z >> n_i >> n_f >> unit;
-        double delta_E{13.6 * z * z * (1.0 / (n_i * n_i) - 1.0 / (n_f * n_f))};
-        if (unit == "j")
+        if (n_i< n_f)
+        {
+            std::cout << "n_i must be greater than n_f" << std::endl;
+            
+        }
+        else
+        {
+        double delta_E{13.6 * z * z * (1.0 / (n_f * n_f) - 1.0 / (n_i * n_i))};
+        if (unit == "J")
         {
             delta_E *= 1.60218e-19;
             std::cout << delta_E << std::endl;
@@ -23,6 +31,8 @@ int main()
         {
             std::cout << "Invalid unit" << std::endl;
         } 
+        }
+        std::cout << "Continue? (y/n): " << std::endl;
         std::cin >> cont;
     }
     return 0;
