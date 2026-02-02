b10b2af9a63ac2007505bfa3a37ef3f8d21473b3
m41250gj <george.jones-6@student.manchester.ac.uk>
2026-02-02 15:40:38 +0000

    working if statements and equations - need to add n_i<n_f condition/ safe type checks but will wait for the canvas stuff to be uploaded for output formatting and sorted out putting std before all io/string instances

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,7 +1,29 @@
 
 #include <iostream>
+#include <string>
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::string cont{"y"};
+    while (cont == "y")
+    {
+        int z, n_i, n_f;
+        std::string unit;
+        std::cin >> z >> n_i >> n_f >> unit;
+        double delta_E{13.6 * z * z * (1.0 / (n_i * n_i) - 1.0 / (n_f * n_f))};
+        if (unit == "j")
+        {
+            delta_E *= 1.60218e-19;
+            std::cout << delta_E << std::endl;
+        }
+        else if (unit == "e")
+        {
+            std::cout << delta_E << std::endl;
+        }
+        else
+        {
+            std::cout << "Invalid unit" << std::endl;
+        } 
+        std::cin >> cont;
+    }
     return 0;
 }
